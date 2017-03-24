2756afab726f172630f6f9654f7327f5b1834028
Rene van der Ark <r.van.der.ark@gmail.com>
2017-03-24 15:44:11 +0100

    fix semi

--- src/client/reducers/dialogs.js
+++ src/client/reducers/dialogs.js
@@ -6,7 +6,7 @@ const initialState = [];
 export default function(state=initialState, action) {
     switch (action.type) {
         case ActionTypes.RECEIVE_DIALOGS:
-            return action.dialogs
+            return action.dialogs;
         default:
     }
 
